7106540c8f88ff497050980d2f9624d909642ef6
Seb290698 <Seb290698@users.noreply.github.com>
2019-12-15 09:17:46 +0000

    automated commit by submit50

--- recover.c
+++ recover.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
     {
         // check if we found the beginning of a JPEG
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff
-        && (buffer[3] & 0xf0) == 0xe0)
+            && (buffer[3] & 0xf0) == 0xe0)
         {
             if (outptr != NULL) // found another JPEG then close the previous file
             {
